57686dd51a0de303dcb35b9e9fb226b9ede21387
weronika_gajda <73773207+weronikagajda@users.noreply.github.com>
2023-10-12 22:19:58 +0200

    Update __init__.py
    update week2

--- __init__.py
+++ __init__.py
@@ -1,22 +1,9 @@
-# import sys
-# import os
-# import bpy
-# import importlib
-
-# blend_dir = os.path.dirname(bpy.data.filepath)
-# if blend_dir not in sys.path:
-#    sys.path.append(blend_dir)
-
 from . import panels, operators
-
-# importlib.reload(panels)
-# importlib.reload(operators)
-# importlib.reload(utilities)
 
 bl_info = {
     "name" : "Citography - Beta",
     "author" : "Wero",
-    "version" : (1, 9),
+    "version" : (1, 10),
     "blender" : (3, 5, 1),
     "locoation" : "View3d > Tool",
     "description": "Extended Mapping Tool",
@@ -24,26 +11,6 @@ bl_info = {
     "wiki_url" : "",
     "catregory" : "3D View",   
 }
-
-#PANEL CITOGRAPHY - START 0.0
-#   OPERATOR 0.1: clean the scene
-
-#PANEL CITOGRAPHY - IMPORT 
-#
-#   PANEL: images
-#   OPERATOR IMPORT IMAGES 1.0: 
-#       OPERATOR 1.1: import one random image 
-#       OPERATOR 1.2: distibute all images on a grid with choosen spacing
-#       OPERATOR 1.3: RESIZE / ROTATE / *BEVEL/WRAP
-#   
-#   PANEL: geoimages
-#   OPERATOR IMPORT 2.0: import geolocated images
-#       OPERATOR 2.1: georeference
-#
-#   PANEL: CSV/GPX data
-#   OPERATOR IMPORT 3: import csv/gps data
-#       OPERATOR 1: import points
-#       OPERATOR 2: make a path
     
 def register():
     panels.register()
@@ -56,6 +23,3 @@ def unregister():
 if __name__ == "__main__":
     register()
     
-
-
-
